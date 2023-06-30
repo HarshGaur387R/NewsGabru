ea8a82c10ee7b44127357c22bfcbc4c2163d5b19
Harsh Gaur <hgaur701@gmail.com>
2023-06-30 16:54:34 +0530

    creating profilebar

--- app/src/items/Profilebar.mjs
+++ app/src/items/Profilebar.mjs
@@ -5,28 +5,26 @@ export default class Profilebar extends Component {
     return (
       <>
         <div className='profilebar-container psn-fixed'>
-          <div >
-            <div className='sec1-prof-container'>
-              <div className='image-container dis-flex jc-center ai-center'>
-                <img src="assets\person_FILL1_wght400_GRAD0_opsz48.svg" alt="profile-pic" />
-              </div>
+          <div className='sec1-prof-container dis-flex ai-center jc-center'>
+            <div className='image-container dis-flex jc-center ai-center'>
+              <img src="assets\person_FILL1_wght400_GRAD0_opsz48.svg" alt="profile-pic" />
             </div>
+          </div>
 
-            <div className='buttons-container'>
-              <button type="button" title='button for signIn'>SignIn</button>
-              <button type="button" title='button for signUp'>SignUp</button>
-            </div>
+          <div className='sec2-prof-container buttons-container dis-flex ai-center jc-center'>
+            <button type="button" title='button for signIn'>Sign In</button>
+            <button type="button" title='button for signUp'>Sign Up</button>
+          </div>
 
-            <hr />
+          <hr />
 
-            <div className='sec2-prof-container'>
-              <ul>
-                <li>Language</li>
-                <li>Settings</li>
-                <li>Contact Us</li>
-                <li>Support</li>
-              </ul>
-            </div>
+          <div className='sec3-prof-container'>
+            <ul>
+              <li>Language</li>
+              <li>Settings</li>
+              <li>Contact Us</li>
+              <li>Support</li>
+            </ul>
           </div>
         </div>
       </>
