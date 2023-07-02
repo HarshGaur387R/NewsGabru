49940fb8de142c9d1236dbae1bf9ba8fb593e527
Harsh Gaur <hgaur701@gmail.com>
2023-07-02 17:07:24 +0530

    adding active class on click of navbar option elements.

--- app/src/items/NavbarOptions.mjs
+++ app/src/items/NavbarOptions.mjs
@@ -1,12 +1,22 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+
+// On click save the index of clicked item into active index. And create a logic for li className . If active index is === li index than turn on the active class
+
 
 export default class NavbarOptions extends Component {
     constructor(props) {
         super(props);
         this.state = {
             scrollStep: 200,
+            activeIndex: null
         };
         this.navbarItemListRef = React.createRef();
+    }
+
+    handleOnListItemCLick(index){
+        this.setState({
+            activeIndex : index
+        })
     }
 
     handleNextClick = () => {
@@ -28,6 +38,12 @@ export default class NavbarOptions extends Component {
     };
 
     render() {
+
+        const items = [
+            'All', 'Sports', 'Crime', 'Economics', 'Geography', 'Space', 'War', 'Politics', 'Celebrities', 'Crypto', 'Programming' ];
+
+        const listItems = items.map((item, index) => <li onClick={()=>this.handleOnListItemCLick(index)} className={this.state.activeIndex === index?'active':''} key={index}>{item}</li>);
+
         return (
             <div>
                 <nav className="navbar2 jc-center ai-center psn-fixed dis-flex">
@@ -38,20 +54,7 @@ export default class NavbarOptions extends Component {
                         onClick={this.handlePrevClick}
                     />
                     <ul className="options-list dis-flex list-none" ref={this.navbarItemListRef}>
-                        <li>All</li>
-                        <li>Sports</li>
-                        <li>Crime</li>
-                        <li>Economics</li>
-                        <li>Geography</li>
-                        <li>Space</li>
-                        <li>War</li>
-                        <li>Politics</li>
-                        <li>Celebrities</li>
-                        <li>Crypto</li>
-                        <li>Programming</li>
-                        <li>Programming</li>
-                        <li>Programming</li>
-                        <li>Programming</li>
+                        {listItems}
                     </ul>
                     <img id="NextButton" src="assets\arrowForward.svg" alt="Next arrow" onClick={this.handleNextClick} />
                 </nav>
